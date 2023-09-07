8f4359d61c4fc5a174b0f0d0e879d67b63c63289
fbicciato <f.bicciato.lavoro@gmail.com>
2023-09-07 23:13:32 +0200

    Aggiungere i file di progetto.

---
+++ TestZPLPrint/SocketManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestZPLPrint
+{
+    internal class SocketManager
+    {
+        private int _port;
+        private string _ip { get; set; }
+        private Socket _socket;
+
+        public SocketManager ForIP(string ip)
+        {
+            _ip = ip;
+            return this;
+        }
+
+        public SocketManager WithPort(int port)
+        {
+            _port = port;
+            return this;
+        }
+
+        public NetworkStream OpenSocket()
+        {
+                   
+            var Ip = IPAddress.Parse(_ip);            
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                _socket.Connect(Ip, _port);
+            }
+            catch (Exception)
+            {
+                return null;
+
+            }
+            
+            if (!_socket.Connected)
+            {
+                return null;
+            }
+            var networkStream = new NetworkStream(_socket);            
+            return networkStream;
+        }
+        public void CloseSocket()
+        {
+            _socket.Close();            
+        }
+
+
+        public bool SendStream(string content)
+        {
+            var networkStream = OpenSocket();
+            if (networkStream==null) return false;
+
+            byte[] dati = Encoding.Default.GetBytes(content);
+            networkStream.Write(dati, 0, dati.Length);
+            networkStream.Flush();            
+            networkStream.Close();
+            CloseSocket();
+            return true;
+            
+        }
+
+
+    }
+}
---
+++ TestZPLPrint/ZPLAction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestZPLPrint
+{
+    internal class ZPLAction
+    {
+
+        private int _port;
+        private string _ip { get; set; }
+
+        public ZPLAction ForIP(string ip)
+        {
+            _ip = ip;
+            return this;
+        }
+
+        public ZPLAction WithPort(int port)
+        {
+            _port = port;
+            return this;
+        }
+
+
+
+
+
+
+        //public void StampaZplEtichetta(Int64 id, string qta)
+        //{
+        //    try
+        //    {   
+
+        //        // Lettura file etichetta segnaposto {0}, ....
+        //        string ZPLString = string.Empty;
+        //        using (StreamReader sr = new StreamReader(HttpContext.Current.Server.MapPath("~/App_Data/etichetta.zpl")))
+        //        {
+        //            ZPLString = sr.ReadToEnd();
+        //            sr.Close();
+        //        }
+        //        // Recupero dati etichetta da stored
+                
+        //        SendToZPL(ZPLString);
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        Console.ForegroundColor = ConsoleColor.Red;
+        //        Console.WriteLine("stampaZpl ", ex.InnerException);
+        //    }
+        //}
+        internal bool SendToZPL(string zplcontent)
+        {
+            var sc = new SocketManager();
+            sc.ForIP(_ip).WithPort(_port);
+            return sc.SendStream(zplcontent);           
+
+        }
+    }
+
+}
